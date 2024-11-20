937e863e53f835ab2bf74829fd7e94d14e5a2e39
Pixmi <admin@pixmi.dev>
2024-11-20 23:41:17 +0800

    small adjustments

--- image-search-script.meta.js
+++ image-search-script.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Image Search Script
 // @namespace   http://tampermonkey.net/
-// @version     1.1.2
+// @version     1.1.3
 // @description Quickly access an intuitive and visually pleasing image search menu with a long right-click on any image.
 // @description:zh-TW 長按滑鼠右鍵，快速呼叫圖片搜尋選單，提供簡潔流暢的使用體驗。
 // @description:zh-CN 长按滑鼠右键，快速呼叫图片搜寻选单，提供简洁流畅的使用体验。
@@ -12,7 +12,7 @@
 // @supportURL  https://github.com/Pixmi/image-search-script/issues
 // @icon        https://raw.githubusercontent.com/Pixmi/image-search-script/main/icon.svg
 // @match       *://*/*
-// @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @require     https://raw.githubusercontent.com/sizzlemctwizzle/GM_config/refs/heads/master/gm_config.js
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_addStyle
