2e99850cb6830fc0e0fc356ca73e3f0e7730490c
Pixmi <admin@pixmi.dev>
2024-11-21 23:51:02 +0800

    update version

--- image-search-script.meta.js
+++ image-search-script.meta.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name        Image Search Script
+// @name:zh-TW  Image Search Script
+// @name:zh-CN  Image Search Script
 // @namespace   http://tampermonkey.net/
-// @version     1.1.5
+// @version     1.1.6
 // @description Long-pressing the right mouse button brings up the image search menu, offering a smooth and concise user experience.
-// @description:zh-TW 長按滑鼠右鍵呼叫圖片搜尋選單，提供流暢且簡潔的使用體驗。
+// @description 長按滑鼠右鍵呼叫圖片搜尋選單，提供流暢且簡潔的使用體驗。
 // @description:zh-CN 长按滑鼠右键呼叫图片搜寻选单，提供流畅且简洁的使用体验。
 // @author      Pixmi
 // @homepage    https://github.com/Pixmi/image-search-script
--- image-search-script.user.js
+++ image-search-script.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name        Image Search Script
+// @name:zh-TW  Image Search Script
+// @name:zh-CN  Image Search Script
 // @namespace   http://tampermonkey.net/
-// @version     1.1.5
+// @version     1.1.6
 // @description Long-pressing the right mouse button brings up the image search menu, offering a smooth and concise user experience.
 // @description:zh-TW 長按滑鼠右鍵呼叫圖片搜尋選單，提供流暢且簡潔的使用體驗。
 // @description:zh-CN 长按滑鼠右键呼叫图片搜寻选单，提供流畅且简洁的使用体验。
