4cdbff1cfa5d01262483ad15417e7e8ca3bd0d4e
Pixmi <admin@pixmi.dev>
2024-11-23 23:40:04 +0800

    add url encode

--- image-search-script.meta.js
+++ image-search-script.meta.js
@@ -3,7 +3,7 @@
 // @name:zh-TW  Image Search Script
 // @name:zh-CN  Image Search Script
 // @namespace   https://github.com/Pixmi/image-search-script
-// @version     1.1.7
+// @version     1.1.8
 // @description Long-pressing the right mouse button brings up the image search menu, offering a smooth and concise user experience.
 // @description:zh-TW 長按滑鼠右鍵呼叫圖片搜尋選單，提供流暢且簡潔的使用體驗。
 // @description:zh-CN 长按滑鼠右键呼叫图片搜寻选单，提供流畅且简洁的使用体验。
@@ -20,6 +20,7 @@
 // @grant       GM_addStyle
 // @grant       GM_registerMenuCommand
 // @grant       GM_xmlhttpRequest
+// @grant       GM_openInTab
 // @connect     ascii2d.net
 // @license     GPL-3.0
 // @run-at      document-body
